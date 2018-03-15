1223d612babc4ea88c6167c311aa9ec142ff97c8
vgr2 <valdeckr@gmail.com>
2018-03-15 10:46:55 -0500

    Added more routes and the test for the roo redirect to rentals

--- tests/acceptance/list-rentals-test.js
+++ tests/acceptance/list-rentals-test.js
@@ -6,6 +6,8 @@ module('Acceptance | my acceptance test', function(hooks) {
   setupApplicationTest(hooks);
 
   test('should show rentals as the home page', async function (assert) {
+    await visit('/');
+      assert.equal(currentURL(), '/rentals', 'should redirect automatically');
   });
 
   test('should link to information about the company.', async function (assert) {
